e8c479b780f0a73c8369cc2f30cfd8ac566ab20e
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-04-01 12:44:37 +0100

    hbs and JS update - dashB, login/out,post, sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,13 @@
 DROP DATABASE IF EXISTS tech_blog_db;
 CREATE DATABASE tech_blog_db;
 USE tech_blog_db;
+
+-- JAWSDB_URL=mysql://ds04i250h051zpye:carz1j05jnn7latm@
+
+-- HOST:ilzyz0heng1bygi8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com
+
+-- PORT:3306/
+
+-- Password	carz1j05jnn7latm
+
+-- DATABASE: ivaisdqq0o2igd3t
