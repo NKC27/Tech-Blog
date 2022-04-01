52b7e975c45de76948a8a126f314e984e98062a4
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-04-01 13:10:09 +0100

    testing

--- models/index.js
+++ models/index.js
@@ -2,10 +2,10 @@ const User = require('./User');
 const Comment = require('./Comment');
 const Post = require('./Post');
 
-User.hasMany(Post, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE',
-  });
+// User.hasMany(Post, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE',
+//   });
   
 User.hasMany(Comment, {
     foreignKey: 'user_id',
