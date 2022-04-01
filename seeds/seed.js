d5ad1b014810a6dd191372ca4549b496087e8993
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-04-01 11:45:48 +0100

    seeds updated & .env file created

---
+++ seeds/seed.js
@@ -0,0 +1,27 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+    await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    await Post.bulkCreate(postData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    await Comment.bulkCreate(commentData, {
+      individualHooks: true,
+      returning: true,
+    });
+  };
+  
+  seedDatabase();
